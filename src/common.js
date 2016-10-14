d2bd493210c0f3ac0db0f2fcbb3b38d19a0c8f4b
lza@arthur <lza@meitu.com>
2016-10-14 20:55:58 +0800

    change the common var

--- src/common.js
+++ src/common.js
@@ -1,12 +1,14 @@
 /**
  * Created by Zhongyi on 3/26/16.
  */
+//
 'use strict';
 class Common {
 
 }
 Common.ELECTRON = 'Electron';
 Common.ELECTRONIC_WECHAT = 'Electronic WeChat';
+Common.ELECTRONIC_QQ = 'Electronic QQ';
 Common.DEBUG_MODE = false;
 Common.WINDOW_SIZE = {
   width: 800,
@@ -22,12 +24,12 @@ Common.WINDOW_SIZE_LOADING = {
 };
 Common.USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2227.1 Safari/537.36';
 
-Common.WEB_WECHAT = 'https://wx.qq.com/';
-Common.GITHUB = 'https://github.com/geeeeeeeeek/electronic-wechat';
-Common.GITHUB_RELEASES = 'https://github.com/geeeeeeeeek/electronic-wechat/releases';
-Common.GITHUB_ISSUES = 'https://github.com/geeeeeeeeek/electronic-wechat/issues';
+Common.WEB_WECHAT = 'https://w.qq.com/';
+Common.GITHUB = 'https://github.com/arthurkiller/electronic-QQ';
+Common.GITHUB_RELEASES = 'https://github.com/arthurkliller/electronic-QQ/releases';
+Common.GITHUB_ISSUES = 'https://github.com/arthurkliller/electronic-QQ/issues';
 Common.GITHUB_API_HOST = 'api.github.com';
-Common.GITHUB_API_RELEASE_LATEST_PATH = '/repos/geeeeeeeeek/electronic-wechat/releases/latest';
+Common.GITHUB_API_RELEASE_LATEST_PATH = '/repos/arthurkliller/electronic-QQ/releases/latest';
 
 Common.UPDATE_ERROR_ELECTRON = 'Failed to get the local version. If you are using debug mode(by `npm start`), this error would happen. Use packed app instead or manually check for updates.\n\n' + Common.GITHUB_RELEASES;
 Common.UPDATE_ERROR_EMPTY_RESPONSE = 'Failed to fetch release info.';
