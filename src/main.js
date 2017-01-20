2c6ca5da1b30358e0821953a3e80232149a7052b
lza@arthur <lza@meitu.com>
2017-01-20 19:31:31 +0800

    set basic version

--- src/main.js
+++ src/main.js
@@ -12,94 +12,52 @@ const WeChatWindow = require('./windows/controllers/wechat');
 const AppTray = require('./windows/controllers/app_tray');
 
 class ElectronicWeChat {
-  constructor() {
-    this.wechatWindow = null;
-    this.splashWindow = null;
-    this.tray = null;
-  }
+    constructor() {
+        this.wechatWindow = null;
+        this.splashWindow = null;
+        this.tray = null;
+    }
 
-  init() {
-    this.initApp();
-    this.initIPC();
-  }
+    init() {
+        this.initApp();
+        this.initIPC();
+    }
 
-  initApp() {//主界面
-      //ready时创建window。。。
-      //when Electron has finished initializing
-    app.on('ready', ()=> {
-      this.createSplashWindow();
-      this.createWeChatWindow();
-      this.createTray();
-    });
-      //activate 展示窗口
-      //which usually happens when the user clicks on the application’s dock icon.
-    app.on('activate', () => {
-      if (this.wechatWindow == null) {
-        this.createWeChatWindow();
-      } else {
-        this.wechatWindow.show();
-      }
-    });
-  };
+    initApp() {//主界面
+        //when Electron has finished initializing
+        app.on('ready', ()=> {
+            this.createWeChatWindow();
+        });
+        //which usually happens when the user clicks on the application’s dock icon.
+        app.on('activate', () => {
+            if (this.wechatWindow == null) {
+                this.createWeChatWindow();
+            } else {
+                this.wechatWindow.show();
+            }
+        });
+        app.on('closed', function() {
+            this.wechatWindow.close();
+        });
+    };
 
-  initIPC() { //loading界面
-    //num是干啥用的？？？(看起来是消息数在任务栏的设定，就是数字)event 呢？ 同样不知道
-    ipcMain.on('badge-changed', (event, num) => {
-        //用于状态变化，需要提示未读消息的时候
-      if (process.platform == "darwin") {
-        app.dock.setBadge(num);
-        if (num) {
-          this.tray.setTitle(` ${num}`);
-        } else {
-          this.tray.setTitle('');
-        }
-      } else if (process.platform == "linux") {
-          app.setBadgeCount(num * 1);
-          this.tray.setUnreadStat((num * 1 > 0)? 1 : 0);
-      }
-    });
-
-      //注册各种事件，用于触发inject
-    ipcMain.on('user-logged', () => {
-      this.wechatWindow.resizeWindow(true, this.splashWindow)
-    });
-
-    ipcMain.on('wx-rendered', (event, isLogged) => {
-      this.wechatWindow.resizeWindow(isLogged, this.splashWindow)
-    });
-
-    ipcMain.on('log', (event, message) => {
-      console.log(message);
-    });
-
-    ipcMain.on('reload', (event, repetitive) => {
-      if (repetitive) {
-        this.wechatWindow.loginState.current = this.wechatWindow.loginState.NULL;
-        this.wechatWindow.connect();
-      } else {
-        this.wechatWindow.loadURL(Common.WEB_WECHAT);
-      }
-    });
-
-    ipcMain.on('update', (event, message) => {
-      let updateHandler = new UpdateHandler();
-      updateHandler.checkForUpdate(`v${app.getVersion()}`, false);
-    });
-  };
+    initIPC() { //loading界面
+        ipcMain.on('reload', (event, message) => {
+            this.wechatWindow.loadURL(Common.WEB_WECHAT);
+        });
+    };
 
     //创建各种窗口
-  createTray() {
-    this.tray = new AppTray(this.splashWindow, this.wechatWindow);
-  }
-
-  createSplashWindow() {
-    this.splashWindow = new SplashWindow();
-    this.splashWindow.show();
-  }
-
-  createWeChatWindow() {
-    this.wechatWindow = new WeChatWindow();
-  }
+    createTray() {
+        this.tray = new AppTray(this.splashWindow, this.wechatWindow);
+    }
+    createSplashWindow() {
+        this.splashWindow = new SplashWindow();
+        this.splashWindow.show();
+    }
+    createWeChatWindow() {
+        this.wechatWindow = new WeChatWindow();
+    }
 }
 
 new ElectronicWeChat().init();
--- src/windows/controllers/wechat.js
+++ src/windows/controllers/wechat.js
@@ -13,116 +13,105 @@ const UpdateHandler = require('../../handlers/update');
 
 //主窗口？
 class WeChatWindow {
-  constructor() {
-    this.loginState = { NULL: -2, WAITING: -1, YES: 1, NO: 0 };
-    this.loginState.current = this.loginState.NULL; //初始化设置为NULL
-    this.inervals = {};
-    this.createWindow();
-  }
-
-  resizeWindow(isLogged, splashWindow) {
-      //根据login状态调整窗口大小，窗口大小不能自主调整，是固定的
-    const size = isLogged ? Common.WINDOW_SIZE : Common.WINDOW_SIZE_LOGIN;
-
-    this.wechatWindow.setResizable(isLogged);
-    this.wechatWindow.setSize(size.width, size.height);
-    if (this.loginState.current === 1 - isLogged || this.loginState.current === this.loginState.WAITING) {
-      splashWindow.hide();
-      this.wechatWindow.show();
-      this.wechatWindow.center();
-      this.loginState.current = isLogged;
-    }
-  }
-
-  createWindow() {
-    this.wechatWindow = new BrowserWindow({
-      title: Common.ELECTRONIC_WECHAT,
-      resizable: true,
-      center: true,
-      show: false,
-      frame: true,
-      autoHideMenuBar: true,
-      icon: path.join(__dirname, '../../../assets/icon.png'),
-      titleBarStyle: 'hidden-inset',
-      webPreferences: {
-        javascript: true,
-        plugins: true,
-        nodeIntegration: false,
-        webSecurity: false,
-        preload: path.join(__dirname, '../../inject/preload.js'),
-      },
-    });
-
-    this.wechatWindow.webContents.setUserAgent(Common.USER_AGENT);
-    if (Common.DEBUG_MODE) {
-      this.wechatWindow.webContents.openDevTools();
+    constructor() {
+        this.loginState = { NULL: -2, WAITING: -1, YES: 1, NO: 0 };
+        this.loginState.current = this.loginState.NULL; //初始化设置为NULL
+        this.inervals = {};
+        this.createWindow();
     }
 
-    this.connect();
+    resizeWindow(isLogged, splashWindow) {
+        //根据login状态调整窗口大小，窗口大小不能自主调整，是固定的
+        const size = isLogged ? Common.WINDOW_SIZE : Common.WINDOW_SIZE_LOGIN;
 
-    this.wechatWindow.webContents.on('will-navigate', (ev, url) => {
-      if (/(.*wx.*\.qq\.com.*)|(web.*\.wechat\.com.*)/.test(url)) return;
-      ev.preventDefault();
-    });
+        this.wechatWindow.setResizable(isLogged);
+        this.wechatWindow.setSize(size.width, size.height);
+        if (this.loginState.current === 1 - isLogged || this.loginState.current === this.loginState.WAITING) {
+            splashWindow.hide();
+            this.wechatWindow.show();
+            this.wechatWindow.center();
+            this.loginState.current = isLogged;
+        }
+    }
 
-    this.wechatWindow.on('close', (e) => {
-      if (this.wechatWindow.isVisible()) {
-        e.preventDefault();
-        this.wechatWindow.hide();
-      }
-    });
+    createWindow() {
+        this.wechatWindow = new BrowserWindow({
+            title: Common.ELECTRONIC_QQ,
+            height: 900,
+            width: 1440,
+            center: true,
+            show: true,
+            frame: true,
+            icon: path.join(__dirname, '../../../assets/icon.png'),
+            webPreferences: {
+                javascript: true,
+                webSecurity: true,
+                allowDisplayingInsecureContent: true,
+                allowRunInsecureContent: true,
+                plugins: true,
+                nodeIntegration: true,
+            },
+        });
 
-    this.wechatWindow.on('page-title-updated', (ev) => {
-      if (this.loginState.current === this.loginState.NULL) {
-        this.loginState.current = this.loginState.WAITING;
-      }
-      ev.preventDefault();
-    });
+        this.wechatWindow.webContents.setUserAgent(Common.USER_AGENT);
+        if (Common.DEBUG_MODE) {
+            this.wechatWindow.webContents.openDevTools();
+        }
 
-    this.wechatWindow.webContents.on('dom-ready', () => {
-      this.wechatWindow.webContents.insertCSS(CSSInjector.commonCSS);
-      if (process.platform === 'darwin') {
-        this.wechatWindow.webContents.insertCSS(CSSInjector.osxCSS);
-      }
+        this.connect();
 
-      if (!UpdateHandler.CHECKED) {
-        new UpdateHandler().checkForUpdate(`v${app.getVersion()}`, true);
-      }
-    });
+        this.wechatWindow.webContents.on('will-navigate', (ev, url) => {
+            if (/(.*wx.*\.qq\.com.*)|(web.*\.wechat\.com.*)/.test(url)) return;
+            ev.preventDefault();
+        });
 
-    this.wechatWindow.webContents.on('new-window', (event, url) => {
-      event.preventDefault();
-      shell.openExternal(new MessageHandler().handleRedirectMessage(url));
-    });
+        this.wechatWindow.on('close', (e) => {
+            if (this.wechatWindow.isVisible()) {
+                e.preventDefault();
+                this.wechatWindow.hide();
+            }
+        });
 
-    this.wechatWindow.webContents.on('will-navigate', (event, url) => {
-      if (url.endsWith('/fake')) event.preventDefault();
-    });
-  }
+        this.wechatWindow.on('page-title-updated', (ev) => {
+            if (this.loginState.current === this.loginState.NULL) {
+                this.loginState.current = this.loginState.WAITING;
+            }
+            ev.preventDefault();
+        });
 
-  loadURL(url) {
-    this.wechatWindow.loadURL(url);
-  }
+        this.wechatWindow.webContents.on('dom-ready', () => {
+            this.wechatWindow.webContents.insertCSS(CSSInjector.commonCSS);
+            if (process.platform === 'darwin') {
+                this.wechatWindow.webContents.insertCSS(CSSInjector.osxCSS);
+            }
 
-  show() {
-    this.wechatWindow.show();
-  }
+            if (!UpdateHandler.CHECKED) {
+                new UpdateHandler().checkForUpdate(`v${app.getVersion()}`, true);
+            }
+        });
 
-  connect() {
-    Object.keys(this.inervals).forEach((key, index) => {
-      clearInterval(key);
-      delete this.inervals[key];
-    });
+        this.wechatWindow.webContents.on('new-window', (event, url) => {
+            event.preventDefault();
+            shell.openExternal(new MessageHandler().handleRedirectMessage(url));
+        });
 
-    this.loadURL(Common.WEB_WECHAT);
-    const int = setInterval(() => {
-      if (this.loginState.current === this.loginState.NULL) {
-        this.loadURL(Common.WEB_WECHAT);
-        console.log('Reconnect.');
-      }
-    }, 5000);
-    this.inervals[int] = true;
-  }
+        this.wechatWindow.webContents.on('will-navigate', (event, url) => {
+            if (url.endsWith('/fake')) event.preventDefault();
+        });
+    }
+
+    loadURL(url) {
+        this.wechatWindow.loadURL(url);
+    }
+
+    show() {
+        this.wechatWindow.show();
+    }
+
+    connect() {
+        this.loadURL(Common.WEB_QQ);
+        console.log('Connectted');
+    }
 }
 
 module.exports = WeChatWindow;
